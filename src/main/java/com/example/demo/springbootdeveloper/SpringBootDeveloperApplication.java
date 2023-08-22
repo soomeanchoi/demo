4f408518499d24d9d3f74537582a02e7c6fcf132
soomeanchoi <suminchoi0123@gmail.com>
2023-08-22 22:57:38 +0900

    08.22

---
+++ src/main/java/com/example/demo/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,10 @@
+package com.example.demo.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
