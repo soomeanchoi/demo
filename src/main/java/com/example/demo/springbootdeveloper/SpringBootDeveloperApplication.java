df4ee17d6376179e8cc4735d687967c89abc7422
soomeanchoi <suminchoi0123@gmail.com>
2023-11-29 22:57:33 +0900

    db연결

--- src/main/java/com/example/demo/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/com/example/demo/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -1,10 +1,14 @@
 package com.example.demo.springbootdeveloper;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
+
+
 }
